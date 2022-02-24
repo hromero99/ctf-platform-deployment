d9a731e53deecb2ca1a0d81bc4d9d48173139120
Hector Romero Lopez <hromero@hromero99.es>
2022-02-24 09:45:11 +0100

    Added dockerfile with vnc and xfce

---
+++ docker/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set password for VNC
+
+mkdir -p /root/.vnc/
+echo $VNCPWD | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+
+# Start VNC server
+
+if [ $VNCEXPOSE = 1 ]
+then
+  # Expose VNC
+  vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH \
+    > /var/log/vncserver.log 2>&1
+else
+  # Localhost only
+  vncserver :0 -rfbport $VNCPORT -geometry $VNCDISPLAY -depth $VNCDEPTH -localhost \
+    > /var/log/vncserver.log 2>&1
+fi
+
+
+/usr/share/novnc/utils/launch.sh --listen $NOVNCPORT --vnc localhost:$VNCPORT 
+
+
